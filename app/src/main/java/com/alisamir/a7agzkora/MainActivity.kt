3359a2916d488e8e417dbb897e33d3cbd15d406d
Ali070 <35662738+Ali070@users.noreply.github.com>
2021-11-16 07:35:04 +0200

    Second Version

--- app/src/main/java/com/alisamir/a7agzkora/MainActivity.kt
+++ app/src/main/java/com/alisamir/a7agzkora/MainActivity.kt
@@ -2,10 +2,34 @@ package com.alisamir.a7agzkora
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.alisamir.a7agzkora.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
+    lateinit var controller: NavController
+    lateinit var appBar:AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolBar)
+        controller = Navigation.findNavController(this,R.id.fragmentContainerView)
+        NavigationUI.setupWithNavController(binding.drawer,controller)
+        appBar = AppBarConfiguration(controller.graph,binding.drawerLayout)
+        setupActionBarWithNavController(controller,appBar)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return controller.navigateUp(appBar)||super.onSupportNavigateUp()
+    }
+    public fun enableDrawer(mode:Int){
+        binding.drawerLayout.setDrawerLockMode(mode)
     }
 }
